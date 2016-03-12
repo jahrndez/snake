f7bf2fba54718cf71710d8b762e520027f9a9ba0
Alex Hernandez <jahrndez@uw.edu>
2016-03-12 15:37:35 -0800

    redesign

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import os
 import re
+import subprocess
 
 ABS_PATH = os.path.realpath(__file__)
 
@@ -13,6 +14,7 @@ class Dir:
         """
 
         # allow for full-paths to be used if they start with '/'
+        self.recursive = recursive
         if dirname[0] == "/":
             self.path = dirname
         else:
@@ -20,19 +22,22 @@ class Dir:
         if not os.path.isdir(self.path):
             raise Exception('specified directory does not exist')
 
-        self.contents = [] # raw filename strings
-        self.targets = {} # for files that became targets
+        self.maps = []
+        self.depenencies = []
+        #self.contents = [] # raw filename strings
+        #self.targets = {} # for files that became targets
 
-        if recursive:
-            for root, dirs, files in os.walk(self.path):
-                for filename in files:
-                    if filename[0] != '.':
-                        self.contents.append(filename)
-        else:
-            root, dirs, files = next(os.walk(self.path))
-            for filename in files:
-                if filename[0] != ".":
-                    self.contents.append(filename)
+        #if recursive:
+        #    for root, dirs, files in os.walk(self.path):
+        #        for filename in files:
+        #            if filename[0] != '.':
+        #                self.contents.append(filename)
+        #else:
+        #    root, dirs, files = next(os.walk(self.path))
+        #    for filename in files:
+        #        if filename[0] != ".":
+        #            self.contents.append(filename)
+
 
     def __getitem__(self, key):
         """Returns the target with the specified name if this directory has it
@@ -48,19 +53,28 @@ class Dir:
         number of wildcards as in (0 or 1). Only dependencies of this directory
         which are matched by a call to map() will be built on build()
         """
-        if "*" in out and "*" not in input:
-            raise Exception("In must have * if out has *")
-        for f in self.contents:
-            matches = re.search(input.replace("*", "(.)+"), f)
-            if matches:
-                self.targets[f] = Target(out.replace("*", matches.group(1)))
+        self.maps.append({"in":input, "out":out})
+        #if "*" in out and "*" not in input:
+        #    raise Exception("In must have * if out has *")
+        #for f in self.contents:
+        #    matches = re.search(input.replace("*", "(.)+"), f)
+        #    if matches:
+        #        self.targets[f] = Target(out.replace("*", matches.group(1)))
 
-    def depends_on(self, *dep):
+    def depends_on(self, *deps):
         """Specifies the dependencies of this directory, i.e. its input in the
         dependency tree.
         """
-        # TODO
-        pass
+        for dep in deps:
+            if isinstance(dep, Target) or isinstance(dep, Dir):
+                self.dependencies.append(dep)
+            elif isinstance(dep, str):
+                if dep[0] == "/":
+                    self.dependencies.append(dep)
+                else:
+                    self.dependencies.append(os.path.join(ABS_PATH, dep))
+            else:
+                raise Exception('dependency must be one of: Target, Dir, or string')
 
     def tool(self, tool):
         """Specify the build tool for this Dir."""
@@ -78,7 +92,25 @@ class Dir:
         Only dependencies which are bound to an output will be built.
         """
         #TODO
+        # contents = scan dir
+        # contents[i] = Target if contents[i] matches, else Leaf
+        # for each target in contents:
+        #    for each d in deps:
+        #        target.depends(d)
+        # build c
         pass
+
+class Leaf:
+    """Wrapper class for files"""
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def has_tool(self):
+        return True
+
+    def build(self):
+        return self.filename
 
 class Target:
     """Root of dependency tree."""
@@ -86,20 +118,27 @@ class Target:
     def __init__(self, out=None):
         """Constructs a new target object, with an output optionally specified.
         """
-        #TODO
-        pass
+        self.out = out
+        self.dependencies = []
 
     def out(self, out):
         """Sets this target's output. This will be the final artifact after
         build() is invoked on this target.
         """
-        #TODO
-        pass
+        self.out = out
 
-    def depends_on(self, *dep):
+    def depends_on(self, *deps):
         """Specify the dependencies of this target."""
-        #TODO
-        pass
+        for dep in deps:
+            if isinstance(dep, Target) or isinstance(dep, Dir):
+                self.dependencies.append(dep)
+            elif isinstance(dep, str):
+                if dep[0] == "/":
+                    self.dependencies.append(Leaf(dep))
+                else:
+                    self.dependencies.append(Leaf(os.path.join(ABS_PATH, dep)))
+            else:
+                raise Exception('dependency must be one of: Target, Dir, or string')
 
     def tool(self, tool):
         """Specify the build tool for this target."""
@@ -109,7 +148,7 @@ class Target:
         """Returns whether a build tool has been previously defined for this
         target.
         """
-        return self.tool is None
+        return self.tool is not None
 
     def build(self, tool=None):
         """Build this target with the specified tool. If no tool is specified
@@ -117,27 +156,43 @@ class Target:
         This target's output must have been previously set either in the
         constructor or in map().
         """
-        #TODO
-        pass
+        #TODO finish
+        if self.out is None:
+            raise Exception('out was never specified')
+        if tool is not None:
+            self.tool = tool
+        if self.tool is None:
+            raise Exception('no tool specified for target')
+
+        ins = [dep.build() if dep.has_tool() else dep.build(self.tool) for dep in self.dependencies]
+        command = self.tool.command()
+        in_string = " ".join(ins)
+
+        if command.contains("{flags}"):
+            command = command.format(inp=in_string, out=self.out, flags=" ".join(self.tool.flags()))
+        else:
+            command = command.format(inp=in_string, out=self.out) + " " + " ".join(self.tool.flags())
+
+        #TODO finish
 
 class Tool:
 
     def __init__(self, command):
         """The specified 'command' will be the actual program executed. The
-        string must contain 2 mandatory placeholders \{in\} and \{out\} and may
+        string must contain 2 mandatory placeholders \{inp\} and \{out\} and may
         contain a third optional placeholder \{flags\}. At build-time, these
         will be replaced with the Target's or Dir's input and output, as well as
         with this tool's flags. If the \{flags\} placeholder is omitted, any
         flags will be appended to the end.
         """
-        self.command = command
+        self.command = command.strip()
 
     def flags(self, *fl):
         """Options specified when running this tool. One flag per argument."""
         self.flags = fl
 
-    def get_command(self):
+    def command(self):
         return command
 
-    def get_flags(self):
+    def flags(self):
         return flags
