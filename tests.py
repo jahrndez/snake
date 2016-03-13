e04c228feb445fe703c10481592a210d7c920528
Alex Hernandez <jahrndez@uw.edu>
2016-03-12 18:04:01 -0800

    tests

--- tests.py
+++ tests.py
@@ -5,16 +5,26 @@ import os
 TEST_FILES_DIR = 'test_files/'
 
 
+class TestUseCases(unittest.TestCase):
+    def setUp(self):
+        out_files = ['bin/main', 'bin/test']
+        # clean
+        for root, dirs, files in os.walk(TEST_FILES_DIR):
+            for filename in files:
+                pass
+
+
 class TestBasic(unittest.TestCase):
     def setUp(self):
         out_files = ['basic']
         # clean
-        root, dirs, files = next(os.walk(TEST_FILES_DIR))
-        for filename in files:
-            if filename[0] != "." and filename.endswith('.o'):
-                os.remove(os.path.join(TEST_FILES_DIR, filename))
-            if filename in out_files:
-                os.remove(os.path.join(TEST_FILES_DIR, filename))
+        for root, dirs, files in os.walk(TEST_FILES_DIR):
+            for filename in files:
+                if filename[0] != "." and filename.endswith('.o'):
+                    os.remove(os.path.join(TEST_FILES_DIR, filename))
+                if filename in out_files:
+                    print os.path.join(TEST_FILES_DIR, filename)
+                    os.remove(os.path.join(TEST_FILES_DIR, filename))
 
     def test_single_c_file(self):
         out_file = TEST_FILES_DIR + 'basic'
