16486e5d09677873c1d748d46f07ba0f31145c37
PTC <pheartheceal@gmail.com>
2016-03-14 22:14:31 -0700

    fix inter

--- demo/intermediate/snakefile.py
+++ demo/intermediate/snakefile.py
@@ -1,9 +1,6 @@
 from snake import Dir, Target, Tool
 
-target = Target('example/a.o')
-target.depends_on('example/a.c')
-target.depends_on('example/b.c')
-my_tool = Tool("gcc {inp} {flags} -o {out}")
-my_tool.flags('-O3 -std=c11')
-target.tool(my_tool)
-target.build()
+my_tool = Tool("gcc {inp} -o {out}", flags=('-O3', '-std=c11'))
+target = Target('example/a')
+target.depends_on('example/a.c', 'example/b.c')
+target.build(my_tool)
