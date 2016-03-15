b789be3f28b2698b9d3ca135bdedb41a93a1aba9
jinxingwang <wjx101220@hotmail.com>
2016-03-14 19:41:33 -0700

    fix examples.py

--- examples.py
+++ examples.py
@@ -2,35 +2,37 @@ from snake import Dir, Target, Tool
 
 EXAMPLES_DIR = 'examples/'
 FLAGS = '-W -Wall -I../.. -Wno-unused-function'
+COMMAND = 'gcc {inp} -o {out} {flags} -lm'
 
-target = Target('call_c_from_js/call_c_from_js')
-target.depends_on('call_c_from_js/call_c_from_js.c')
-target.depends_on('v7.c')
-my_tool = Tool("gcc {inp} -o {out} {flags} -lm")
+target = Target(EXAMPLES_DIR + 'call_c_from_js/call_c_from_js')
+target.depends_on(EXAMPLES_DIR + 'call_c_from_js/call_c_from_js.c')
+target.depends_on(EXAMPLES_DIR + 'v7.c')
+my_tool = Tool(COMMAND)
 my_tool.flags(FLAGS)
 target.tool(my_tool)
 target.build()
 
-target = Target('call_js_from_c/call_js_from_c')
-target.depends_on('call_js_from_c/call_js_from_c.c')
-target.depends_on('v7.c')
-my_tool = Tool("gcc {inp} -o {out} {flags} -lm")
+target = Target(EXAMPLES_DIR + 'call_js_from_c/call_js_from_c')
+target.depends_on(EXAMPLES_DIR + 'call_js_from_c/call_js_from_c.c')
+target.depends_on(EXAMPLES_DIR + 'v7.c')
+my_tool = Tool(COMMAND)
 my_tool.flags(FLAGS)
 target.tool(my_tool)
 target.build()
 
-target = Target('js_oop/js_oop')
-target.depends_on('js_oop/js_oop.c')
-target.depends_on('v7.c')
-my_tool = Tool("gcc {inp} -o {out} {flags} -lm")
+target = Target(EXAMPLES_DIR + 'js_oop/js_oop')
+target.depends_on(EXAMPLES_DIR + 'js_oop/js_oop.c')
+target.depends_on(EXAMPLES_DIR + 'v7.c')
+my_tool = Tool(COMMAND)
 my_tool.flags(FLAGS)
 target.tool(my_tool)
 target.build()
 
-target = Target('load_json_config/load_json_config')
-target.depends_on('load_json_config/load_json_config.c')
-target.depends_on('v7.c')
-my_tool = Tool("gcc {inp} -o {out} {flags} -lm")
+target = Target(EXAMPLES_DIR + 'load_json_config/load_json_config')
+target.depends_on(EXAMPLES_DIR + 'load_json_config/load_json_config.c')
+target.depends_on(EXAMPLES_DIR + 'v7.c')
+my_tool = Tool(COMMAND)
+
 my_tool.flags(FLAGS)
 target.tool(my_tool)
 target.build()
